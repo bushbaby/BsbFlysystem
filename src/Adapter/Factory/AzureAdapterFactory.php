0a783d4b0e024280c3ebfa43a98d822aefca14df
Janis Peisenieks <janis@peisenieks.lv>
2015-03-25 13:37:29 +0200

    Add Azure adapter; Update composer.json to include repository required by Azure SDK; Update dist file to show example usage.

---
+++ src/Adapter/Factory/AzureAdapterFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace BsbFlysystem\Adapter\Factory;
+
+use WindowsAzure\Common\ServicesBuilder;
+use League\Flysystem\Azure\AzureAdapter as Adapter;
+use UnexpectedValueException;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class AzureAdapterFactory extends AbstractAdapterFactory implements FactoryInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function doCreateService(ServiceLocatorInterface $serviceLocator)
+    {
+        $endpoint = sprintf('DefaultEndpointsProtocol=https;AccountName=%s;AccountKey=%s', $this->options['account-name'], $this->options['account-key']);
+        $blobRestProxy = ServicesBuilder::getInstance()->createBlobService($endpoint);
+        $adapter = new Adapter($blobRestProxy, $this->options['container']);
+
+        return $adapter;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function validateConfig()
+    {
+        if (!isset($this->options['account-name'])) {
+            throw new UnexpectedValueException("Missing 'account-name' as option");
+        }
+        if (!isset($this->options['account-key'])) {
+            throw new UnexpectedValueException("Missing 'account-key' as option");
+        }
+        if (!isset($this->options['container'])) {
+            throw new UnexpectedValueException("Missing 'container' as option");
+        }
+    }
+}
