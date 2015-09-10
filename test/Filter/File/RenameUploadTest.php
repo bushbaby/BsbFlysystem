5335667ab659f40dfc5abb993fdc64f254e8a85a
Vytautas Stankus <svycka@gmail.com>
2015-09-10 13:27:41 +0300

    Added tests for RenameUpload file filter

---
+++ test/Filter/File/RenameUploadTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace BsbFlysystemTest\Filter\File;
+
+use BsbFlysystem\Filter\File\RenameUpload;
+use BsbFlysystemTest\Framework\TestCase;
+use Prophecy\Argument;
+
+class RenameUploadTest extends TestCase
+{
+    protected $filesystem;
+
+    public function setUp()
+    {
+        $this->filesystem = $this->prophesize('League\Flysystem\FilesystemInterface');
+    }
+
+    public function testCanUploadFile()
+    {
+        $path = 'path/to/file.txt';
+        $this->filesystem->putStream($path, Argument::any())
+            ->willReturn(true)
+            ->shouldBeCalled();
+        $this->filesystem->has($path)
+            ->willReturn(false);
+
+        $filter = new RenameUpload([
+            'target' => $path,
+            'filesystem' => $this->filesystem->reveal()
+        ]);
+
+        $key = $filter->filter(__DIR__ . '/../../Assets/test.txt');
+        $this->assertEquals($path, $key);
+    }
+
+    public function testWillThrowExceptionWithInvalidConstructorParams()
+    {
+        $this->setExpectedException('Zend\Filter\Exception\InvalidArgumentException');
+        new RenameUpload('something');
+    }
+
+    public function testWillThrowExceptionWhenFilesystemNotSet()
+    {
+        $filter = new RenameUpload([
+            'target' => 'path/to/file.txt',
+        ]);
+
+        $this->setExpectedException('UnexpectedValueException');
+        $filter->filter(__DIR__ . '/../../Assets/test.txt');
+    }
+
+    public function testWillThrowExceptionWhenFileExists()
+    {
+        $path = 'path/to/file.txt';
+        $this->filesystem->has($path)
+            ->willReturn(true)
+            ->shouldBeCalled();
+
+        $filter = new RenameUpload([
+            'target' => $path,
+            'overwrite' => false,
+            'filesystem' => $this->filesystem->reveal()
+        ]);
+
+        $this->setExpectedException('Zend\Filter\Exception\InvalidArgumentException', "File 'path/to/file.txt' could not be uploaded. It already exists.");
+        $filter->filter(__DIR__ . '/../../Assets/test.txt');
+    }
+}
