cc7e7bd2beefe2340b026db9ea3397d36ae92d15
Vytautas Stankus <svycka@gmail.com>
2017-08-24 15:12:12 +0300

    Added tests for ConfigProvider

---
+++ test/ConfigProviderTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BsbFlysystemTest;
+
+use BsbFlysystem\Module;
+use BsbFlysystemTest\Framework\TestCase;
+
+class ConfigProviderTest extends TestCase
+{
+    public function testConfigProviderGetConfig()
+    {
+        $config = (new \BsbFlysystem\ConfigProvider())();
+
+        $this->assertNotEmpty($config);
+    }
+
+    public function testConfigEqualsToModuleConfig()
+    {
+        $moduleConfig = (new Module())->getConfig();
+        $config = (new \BsbFlysystem\ConfigProvider())();
+
+        $this->assertEquals($moduleConfig['service_manager'], $config['dependencies']);
+        $this->assertEquals($moduleConfig['bsb_flysystem'], $config['bsb_flysystem']);
+    }
+}
